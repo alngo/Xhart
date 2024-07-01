beb1a5d8ce68775f44040473467ffdaf13fe09ce
Alngo <alxengo.pro@gmail.com>
2024-07-01 07:56:59 +0200

    Implement basic axum hello world
    add an utility script watch

--- src/registration/src/main.rs
+++ src/registration/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::ptr::null;
+
+use axum::{
+    routing::get,
+    http::StatusCode,
+    Router, extract::{Request, Path}
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(greet))
+        .route("/:name", get(greet_name));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn greet() -> (StatusCode, String) {
+    (StatusCode::OK, "Hello world!".to_string())
+}
+
+async fn greet_name(Path(name): Path<String>) -> (StatusCode, String) {
+    (StatusCode::OK, format!("Hello {}!", name))
 }
